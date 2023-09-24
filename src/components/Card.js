8da862f1d436b1e9cac94573c904c6e82657caad
Oksana Pichugina <okkksanaPichuginna@yandex.ru>
2023-09-24 11:21:56 +0300

    iter3

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,7 +7,6 @@ export class Card {
     ownerId,
     handleLikeClick
   ) {
-    //console.log(data);
     this._text = data.name;
     this._link = data.link;
     this._templateSelector = templateSelector;
@@ -20,9 +19,12 @@ export class Card {
     this._handleDelete = handleDelete;
     this._cardOwnerId = data.owner._id;
     this._ownerId = ownerId;
+    
     this._quantityLikes = data.likes.length;
     this._handleLikeClick = handleLikeClick;
     this._likes = data.likes;
+    
+    this._data = data;
   }
 
   _getTemplate() {
@@ -42,10 +44,10 @@ export class Card {
       .querySelector(".element__name")
       .querySelector(".element__text").textContent = this._text;
     this._elemImage.alt = this._text;
-    this._checkLike();
     this._element
       .querySelector(".element__like")
       .querySelector(".element__like_count").textContent = this._quantityLikes;
+    this._checkLike();
     return this._element;
   }
   // метод удаления карточки
@@ -64,27 +66,30 @@ export class Card {
 
   // метод лайка карточки
   like() {
-    console.log(this._quantityLikes);
+    // console.log(this._quantityLikes);
     let bool;
     if (this._buttonLike.classList.contains("element__like_button_active")) {
       bool = true;
     } else {
       bool = false;
     }
+    //console.log(bool);
+   //console.log(this._data)
     this._handleLikeClick(this._id, bool);
   }
 
   likeCard(quantity) {
-    console.log("сработало");
     this._buttonLike.classList.toggle("element__like_button_active");
     this._element
       .querySelector(".element__like")
       .querySelector(".element__like_count").textContent = quantity;
+      console.log(this._data)
   }
 
   _checkLike() {
     this._likes.forEach((like) => {
-      if ((this._cardOwnerId = like._id)) {
+      console.log(like);
+      if ((this._ownerId == like._id)) {
         this._buttonLike.classList.add("element__like_button_active");
       }
     });
